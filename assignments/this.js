3bf87d61a277ad35675155dd5176721699bb7cdf
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-06-26 01:01:35 -0600

    The four principles of THIS

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. this is used in other languages but it worksdifferently in JS compared to others 
+* 2. this is used instead of the object name
+* 3. using this gives you all of the objects keys and values
+* 4. when using this it doesnt matter where the function is written but is !important to where and when the function is called
 *
 * write out a code example of each explanation above
 */
